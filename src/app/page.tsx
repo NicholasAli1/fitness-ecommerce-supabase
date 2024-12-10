c1f09951fdf6363c6a3a06748e8965f38fc1cce5
Nicholas Ali <111253105+NicholasAli1@users.noreply.github.com>
2024-12-09 22:00:16 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -50,12 +50,6 @@ const HomePage = async () => {
       {/* New Products */}
       <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
         <h1 className="text-2xl">New Products</h1>
-        <Suspense fallback={<Skeleton />}>
-          <ProductList
-            categoryId={newCategory ? newCategory.id : 2}
-            limit={4}
-          />
-        </Suspense>
       </div>
     </div>
   );
